d8a3367760b2f1d7f1059c5dee034e031b7b8367
LimboLam <liam.xiong24@student.cart.org>
2025-04-09 08:38:44 -0700

    stuff

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const input = document.getElementById("input").value;
+const input = document.getElementById("input");
+const text = document.getElementById("text");
+let evaluation;
 
 function palindrome() {
     let palindrome = "";
     
-    for (let i = 1; i <= input.length; i++) {
-        palindrome += input[input.length - i];
+    for (let i = 1; i <= input.value.length; i++) {
+        palindrome += input.value[input.value.length - i];
     }
 
-    console.log(palindrome);
+    if (palindrome === input.value) {
+        evaluation = true;
+        console.log(true);
+    } else {
+        evaluation = false;
+        console.log(false);
+    }
+
+    text.textContent = evaluation;
+    return;
 }
